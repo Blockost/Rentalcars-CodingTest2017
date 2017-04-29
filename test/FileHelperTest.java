b9986fc6df3845605196e39b729cc2d969c33329
Simon Espigolé <espigolesi@eisti.eu>
2017-04-29 18:07:49 +0100

    Add test for FileHelper class

---
+++ test/FileHelperTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import java.io.BufferedReader;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by blockost on 29/04/17.
+ */
+public class FileHelperTest {
+    private BufferedReader reader;
+
+    @Test
+    public void testFileNotFound() {
+        reader = FileHelper.readFile("blahblahtest.json");
+        assertNull(reader);
+    }
+
+    @Test
+    public void testFileFound() {
+        reader = FileHelper.readFile("data/vehicles.json");
+        assertNotNull(reader);
+    }
+
+
+}
